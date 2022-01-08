a00931728e8b725669cc08613d7aeee6fd53fcaf
reyarzhan <93340670+reyarzhan@users.noreply.github.com>
2022-01-08 14:13:07 +0300

    accuracy

--- code/main.py
+++ code/main.py
@@ -8,6 +8,22 @@ def PrintWithSleep(string, sleep_time):
 
 orig_text = GetText();
 
+
+def Accuracy(pattern, result):
+    len1 = len(pattern)
+    len2 = len(result)
+    penalty = [[0 for j in range(len2+1)] for i in range(len1+1)]
+    for i in range (len1+1):
+        penalty[i][0] = i   
+    for j in range (len2+1):
+        penalty[0][j] = j
+    for i in range(1, len1+1):
+        for j in range(1, len2+1):
+            same = (pattern[i-1] != result[j-1])
+            penalty[i][j] = min(penalty[i][j-1]+1, min(penalty[i-1][j]+1, penalty[i-1][j-1]+same))
+    return penalty[len1][len2]
+
+
 PrintWithSleep("Are you ready?", 1)
 PrintWithSleep(orig_text, 0.5)
 PrintWithSleep("3", 0.5)
@@ -28,5 +44,6 @@ if (orig_text == your_text):
     print("Well done")
 else:
     print("Practice more kid")
+print("Accuracy", Accuracy(orig_text, your_text))
 print("Time spent", end_time - start_time)
 
