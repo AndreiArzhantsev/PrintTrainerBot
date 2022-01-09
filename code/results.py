93a3540e55f35cb1dc248219a42ad8a2bea08773
reyarzhan <93340670+reyarzhan@users.noreply.github.com>
2022-01-09 19:33:28 +0300

    Update and rename accuracy.py to results.py

--- code/results.py
+++ code/results.py
@@ -12,3 +12,10 @@ def Accuracy(pattern, result):
             penalty[i][j] = min(penalty[i][j-1]+1, min(penalty[i-1][j]+1, penalty[i-1][j-1]+same))
     return penalty[len1][len2]
 
+def results(orig_text, your_text, time, book):
+    accuracy_result = Accuracy(orig_text.strip(), your_text.strip())
+    print("You made", accuracy_result, 2, "typos")
+    print("Your accuracy is: ", round(100 - 100 * accuracy_result / len(orig_text), 2), "%", sep='')
+    print("Time spent:", round(time, 2), "sec")
+    print("Your speed: ", round(len(orig_text) / time, 2), "symbols/sec")
+    print("This text was from:", book)
