583b5b8ee8a4da6dc1903dd8236c115dadfefebb
reyarzhan <93340670+reyarzhan@users.noreply.github.com>
2022-01-11 11:29:56 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,9 +1,12 @@
 import sys
 import time
 import telebot
+import emoji
+from emoji import emojize
 from telebot import custom_filters
 from get_text import *
-from results import Accuracy
+from result import Accuracy
+
 
 
 class States:
@@ -16,7 +19,7 @@ class States:
     end_time = 0
 
 
-# token =
+token = "5089220566:AAGM006zFa-b2f2RniRRmNrbudcZY8_TwmA"
 bot = telebot.TeleBot(token)
 
 
@@ -38,7 +41,9 @@ Let me introduce myself. I am quite simple bot. All I can do is help you to prac
 @bot.message_handler(commands=['train'])
 def StartTraining(message):
     bot.set_state(message.from_user.id, States.lang)
-    bot.send_message(message.from_user.id, "Select the text language:\n1: English\n2: Русский\n")
+    bot.send_message(message.from_user.id, str(chr(0x1F524)) + "Select the text language:\n"+
+        "1:English"  + "\n" +
+        "2:Русский")
 
 
 @bot.message_handler(state="*", commands=['cancel'])
@@ -55,7 +60,7 @@ def Cancel(message):
 @bot.message_handler(state=States.lang)
 def SetLanguage(message):
     if (message.text in {"1", "2"}):
-        bot.send_message(message.from_user.id, "Select the size of the text:\n1: 20-50 words\n2: 50-100 words\n3: 100-200 words")
+        bot.send_message(message.from_user.id, str(chr(0x1F4DA)) + "Select the size of the text:\n1: 20-50 words\n2: 50-100 words\n3: 100-200 words")
         bot.set_state(message.from_user.id, States.size)
         with bot.retrieve_data(message.from_user.id) as data:
             data['lang'] = message.text
@@ -73,17 +78,17 @@ def SetRemaining(message):
             data['orig_text'] = GetText(source, data['size'])
             data['book'] = NameOfTheBook(source)
 
-            bot.send_message(message.from_user.id, "Are you ready?")
+            bot.send_message(message.from_user.id, str(chr(0x026A1)) + "Are you ready?")
             time.sleep(1)
             bot.send_message(message.from_user.id, data['orig_text'])
             time.sleep(1.5)
-            bot.send_message(message.from_user.id, "3")
-            time.sleep(0.5)
-            bot.send_message(message.from_user.id, "2")
-            time.sleep(0.5)
-            bot.send_message(message.from_user.id, "1")
-            time.sleep(0.5)
-            bot.send_message(message.from_user.id, "Start")
+            bot.send_message(message.from_user.id, str(chr(0x1F534)))
+            time.sleep(0.3)
+            bot.send_message(message.from_user.id, str(chr(0x1F7E1)))
+            time.sleep(0.3)
+            bot.send_message(message.from_user.id, str(chr(0x1F7E2)))
+            time.sleep(0.3)
+            bot.send_message(message.from_user.id, str(chr(0x1F680)) + "Start")
             data['start_time'] = time.time()
     else:
         bot.send_message(message.from_user.id, "Please select 1 or 2 or 3")
@@ -96,11 +101,12 @@ def GetUserText(message):
         data['your_text'] = message.text
         duration = data['end_time'] - data['start_time']
         accuracy_result = Accuracy(data['orig_text'].strip(), data['your_text'].strip())
-        bot.send_message(message.from_user.id, "You made " + str(accuracy_result) + " typos")
-        bot.send_message(message.from_user.id, "Your accuracy is: " + str(100 - round(100 * accuracy_result / len(data['orig_text']), 2)) + "%")
-        bot.send_message(message.from_user.id, "Time spent: " + str(round(duration, 2)) + " sec")
-        bot.send_message(message.from_user.id, "Your speed: " + str(round(len(data['orig_text']) / duration, 2)) + " symbols/sec")
-        bot.send_message(message.from_user.id, "This text was from: " + data['book'])
+        bot.send_message(message.from_user.id,
+            str(chr(0x1F5D1)) + "You made " + str(accuracy_result) + " typos" + "\n" +
+            str(chr(0x1F3AF)) + "Your accuracy: " + str(100 - round(100 * accuracy_result / len(data['orig_text']), 2)) + "%" + "\n" + 
+            str(chr(0x0231B)) + "Time spent: " + str(round(duration, 2)) + " sec" + "\n" +
+            str(chr(0x1F6B4)) + "Your speed: " + str(round(len(data['orig_text']) / duration, 2)) + " symbols/sec" + "\n" +
+            str(chr(0x1F4D7)) + "This text was from: " + data['book'])
     bot.delete_state(message.from_user.id)
 
 
@@ -114,4 +120,3 @@ def ReportUnknownMsg(message):
 
 bot.add_custom_filter(custom_filters.StateFilter(bot))
 bot.infinity_polling(skip_pending=True)
-
